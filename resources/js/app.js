1899832b0fae94d435e3eaad8efc9ef6ba553ed2
Oscar Diaz <o-pipe@hotmail.com>
2019-05-16 10:45:25 -0500

    Instalacion webpack laravel mix con los assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,94 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
+new Vue({
     el: '#app',
-});
+    created: function(){
+        this.getAll()
+        this.getAllVotes()
+    },
+    data: {
+        superhero: [],
+        pagination: {
+            'total': 0,
+            'current_page': 0,
+            'per_page': 0,
+            'last_page': 0,
+            'from': 0,
+            'to': 0
+        },
+        fillsuperhero: {'id': '', 'name': '', 'picture': '', 'publisher':'', 'info':'info'},
+        votesSuperhero: [],
+    },
+    computed: {
+		isActived: function() {
+			return this.pagination.current_page;
+		},
+		pagesNumber: function() {
+			if(!this.pagination.to){
+				return [];
+			}
+
+			var from = this.pagination.current_page - this.offset; 
+			if(from < 1){
+				from = 1;
+			}
+
+			var to = from + (this.offset * 2); 
+			if(to >= this.pagination.last_page){
+				to = this.pagination.last_page;
+			}
+
+			var pagesArray = [];
+			while(from <= to){
+				pagesArray.push(from);
+				from++;
+			}
+			return pagesArray;
+		}
+	},
+    methods: {
+        getAll: function(page){
+            var urlsuperhero = "api/superheros?page=" + page
+            axios.get(urlsuperhero).then(response => {
+                this.superhero = response.data.superheros.data
+                this.pagination = response.data.pagination
+            })
+        },
+        localStorageState: function(id,state){
+            localStorage.setItem(id, state);
+            if(state == "like"){
+                toastr.success("me gusta")
+            }else{
+                toastr.error("no me gusta")
+            }
+        },
+        viewSuperhero: function(superhero){
+            this.fillsuperhero.id = superhero.id
+            this.fillsuperhero.name = superhero.name
+            this.fillsuperhero.picture = superhero.picture
+            this.fillsuperhero.publisher = superhero.publisher
+            this.fillsuperhero.info = superhero.info
+
+            $("#view").modal("show")
+        },
+        voteSuperhero: function(superhero){
+            var urladdvote = 'addVote'
+            axios.get(urladdvote, {
+                params: {
+                  id: superhero
+                }
+                }).then(response => {
+                    $("#view").modal("hide")
+                    toastr.success("Voto Agregado!!")
+            })
+        },
+        getAllVotes: function(){
+            var urlgetvotes = "votesSuperheros"
+            axios.get(urlgetvotes).then(response => {
+                this.votesSuperhero = response.data
+            })
+        },
+        changePage: function(page) {
+			this.pagination.current_page = page;
+			this.getAll(page);
+		}
+    }
+})
